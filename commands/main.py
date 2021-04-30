43ba63843eea336164a6ff3530650b02501dab4e
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-04-29 23:19:44 -0400

    Create initial register command script

---
+++ commands/main.py
@@ -0,0 +1,46 @@
+"""A Script to create and delete slash commands."""
+import os
+import requests
+
+
+slash_color = lambda: make_request(
+    endpoint="commands",
+    json={
+        "name": "color",
+        "description": "Identify and display a color that you pass in",
+        "options": [
+            {
+                "name": "color",
+                "description": "The color, in any format.",
+                "type": 3,
+                "required": True,
+            },
+            {
+                "name": "format",
+                "description": "The format to display the color in.",
+                "type": 3,
+                "required": False,
+                "choices": [
+                    {"name": "Name", "value": "name"},
+                    {"name": "HEX", "value": "hex"},
+                    {"name": "RGB", "value": "rgb"},
+                    {"name": "HSL", "value": "hsl"},
+                    {"name": "CMYK", "value": "cmyk"},
+                ],
+            },
+        ],
+    },
+)
+
+
+def make_request(endpoint, json):
+    r = requests.post(
+        url=f"https://discord.com/api/v8/applications/{os.getenv('APPLICATION_ID')}/{endpoint}",
+        headers={"Authorization": f"Bot {os.getenv('BOT_TOKEN')}"},
+        json=json,
+    )
+    print(f"{json['name']}:", r.status_code)
+
+
+if __name__ == "__main__":
+    slash_color()
